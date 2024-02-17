721b91869d3836c80c5f71ac48aef85d8ed0fd1d
Hunter Jennings <jenningsdhunter@gmail.com>
2024-02-17 12:57:45 -0500

    cleanup markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from '@/components/ui/button';
+import { ComboboxDemo } from '@/components/ui/combobox';
+import { Separator } from '@/components/ui/separator';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="space-y-3 p-6">
+      <h1>Combobox in Dialog (modal mode)</h1>
+      <section className="space-y-3 p-6">
+        <h2 className="font-medium">In Dialog</h2>
+        <p>Scroll is broken</p>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button>Open</Button>
+          </SheetTrigger>
+          <SheetContent className="w-[400px] sm:w-[540px]">
+            <SheetHeader>
+              <SheetTitle>Are you absolutely sure?</SheetTitle>
+              <SheetDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </SheetDescription>
+            </SheetHeader>
+            <ComboboxDemo />
+          </SheetContent>
+        </Sheet>
+      </section>
+      <Separator />
+      <section className="space-y-3 p-6">
+        <h2 className="font-medium">Outside Dialog</h2>
+        <p>Scroll is not broken</p>
+        <ComboboxDemo />
+      </section>
+    </div>
+  );
 }
 
-export default App
+export default App;
